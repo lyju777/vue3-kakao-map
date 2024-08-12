335757a435bb08e5a493006d1b129cdb39ca0433
lyju777 <lyju777@hanmail.net>
2024-08-12 11:05:15 +0900

    router 경로 제외

--- src/router/routes.js
+++ src/router/routes.js
@@ -4,11 +4,6 @@ const routes = [
     component: () => import("layouts/MainLayout.vue"),
     children: [{ path: "", component: () => import("pages/MainPage.vue") }],
   },
-
-  {
-    path: "/:catchAll(.*)*",
-    component: () => import("pages/ErrorNotFound.vue"),
-  },
 ];
 
 export default routes;
